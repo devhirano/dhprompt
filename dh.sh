4af0ed0143348ccf082a5b03b36da0cf768f02ef
devhirano <devhirano@gmail.com>
2017-01-14 12:30:44 +0900

    add BAD_KAOMOJI randomization and little fix

--- dh.sh
+++ dh.sh
@@ -21,29 +21,40 @@
 # configuration for dhprompt
 __FULLHOST="no"
 __FETCH_CHECK="true"
+__GOOD_KAOMOJI_SHOW="false"
+__GOOD_KAOMOJI=(":)")
+__BAD_KAOMOJI_RANDOM="true"
+__BAD_KAOMOJI=(":(" "(#\\\`_>´)" "(´-ω-\\\`)" "(;ω;)" "(ﾉД\\\`)" "┐(´д\\\`)┌") 
 
 
-#SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-SCRIPT_DIR="$HOME/.dhprompt"
 
-mkdir -p $SCRIPT_DIR
+#-----------------------------------------------------------
+# kaomoji
+__ARRAY_SIZE=${#__BAD_KAOMOJI[*]}
+__ARRAY_RAND=`expr $RANDOM % ${__ARRAY_SIZE}`
 
-source $SCRIPT_DIR/git-completion.bash
+
+# dir
+__SCRIPT_DIR="$HOME/.dhprompt"
+
+mkdir -p $__SCRIPT_DIR
+
+source $__SCRIPT_DIR/git-completion.bash
 if [ $? -ne 0 ];then
-  curl -o $SCRIPT_DIR/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-  source $SCRIPT_DIR/git-completion.bash
+  curl -o $__SCRIPT_DIR/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+  source $__SCRIPT_DIR/git-completion.bash
 fi
 
-source $SCRIPT_DIR/git-prompt.sh
+source $__SCRIPT_DIR/git-prompt.sh
 if [ $? -ne 0 ];then
-  curl -o  $SCRIPT_DIR/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-  source $SCRIPT_DIR/git-prompt.sh
+  curl -o  $__SCRIPT_DIR/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+  source $__SCRIPT_DIR/git-prompt.sh
 fi
 
 grep "dh.sh" $HOME/.bashrc > /dev/null
 if [ $? -ne 0 ];then
-  FILEPATH=`readlink -f dh.sh`
-  echo "source $FILEPATH " >> $HOME/.bashrc
+  __FILEPATH=`readlink -f dh.sh`
+  echo "source $__FILEPATH " >> $HOME/.bashrc
 fi
 
 ######################################################
@@ -56,11 +67,11 @@ GIT_PS1_SHOWUPSTREAM=AUTO
 ######################################################
 
 
-ISROOT="#"
+__ISROOT="#"
 id | grep 'uid=0(root)' 1>/dev/null 2>/dev/null
 if [ "$?" != "0" ]
 then
-  ISROOT="$"
+  __ISROOT="$"
 fi
 
 __SHORTHOSTNAME=`hostname`
@@ -111,12 +122,23 @@ function exitstatus {
 
     if [ "${EXITSTATUS}" -eq 0 ]
     then
-       # PS1="${PROMPT} ${BOLD}${GREEN}:)${OFF}$(__git_ps1)]\$ "
-       PS1="${PROMPT}${PROXYVAR}${BOLD}${GREEN}:)${OFF}$(__git_ps1) ${ISROOT} "
+      # PS1="${PROMPT} ${BOLD}${GREEN}:)${OFF}$(__git_ps1)]\$ "
+    
+      if [ "$__GOOD_KAOMOJI_SHOW" == "true" ];then
+        PS1="${PROMPT}${PROXYVAR}${BOLD}${GREEN}${__GOOD_KAOMOJI[0]}${OFF}$(__git_ps1) ${__ISROOT} "
+      else
+        PS1="${PROMPT}${PROXYVAR}${BOLD}${GREEN}${OFF}$(__git_ps1)${__ISROOT} "
+      fi
     else
-       # PS1="${PROMPT} ${BOLD}${RED}:(${OFF}$(__git_ps1)]\$ "
-       # PS1="${PROMPT}${PROXYVAR}${BOLD}${RED}(#\\\`_>´) <($?) ${OFF}$(__git_ps1) ${ISROOT} "
-       PS1="${PROMPT}${PROXYVAR}${BOLD}${RED}:(${OFF}$(__git_ps1) ${ISROOT} "
+      if [ "$__BAD_KAOMOJI_RANDOM" == "true" ];then
+        # __ARRAY_RAND=`expr $RANDOM % ${__ARRAY_SIZE}`
+        __BAD_KAOMOJI_SHOW=${__BAD_KAOMOJI[${__ARRAY_RAND}]}
+      else
+        __BAD_KAOMOJI_SHOW=${__BAD_KAOMOJI[0]}
+      fi
+
+      # PS1="${PROMPT}${PROXYVAR}${BOLD}${RED}:(${OFF}$(__git_ps1) ${__ISROOT} "
+      PS1="${PROMPT}${PROXYVAR}${BOLD}${RED}${__BAD_KAOMOJI_SHOW}${OFF}$(__git_ps1) ${__ISROOT} "
     fi
 
     PS2="${BOLD}>${OFF} "
